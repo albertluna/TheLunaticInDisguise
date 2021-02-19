de2600e16fd5e608cab6fac1fc5e9b98fda712c0
DESKTOP-QR76ELR\david <davidbassolscolls@gmail.com>
2021-02-19 19:18:58 +0100

    arrastrar
    Signed-off-by: DESKTOP-QR76ELR\david <davidbassolscolls@gmail.com>

---
+++ Assets/Scripts/Mouobj.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Mouobj : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    private CanvasGroup canvasGroup;
+    private RectTransform rectTransform;
+    private Vector2 startPosition;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // check if the card has been placed in a slot
+        detectobj slot = GetComponentInParent<detectobj>();
+        if (!slot)
+        {
+            transform.position = startPosition;
+            canvasGroup.blocksRaycasts = true;
+        }
+    }
+}
---
+++ Assets/Scripts/detectobj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class detectobj : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            RectTransform cardRect = eventData.pointerDrag.GetComponent<RectTransform>();
+            cardRect.SetParent(transform, false);
+            cardRect.transform.position = transform.position;
+        }
+    }
+}
