b3528b68ededc319abe91c7e65258a152ae26474
albert <albert.luna@students.salle.url.edu>
2021-05-21 19:37:17 +0200

    bugs v3 - crèdits finals

--- Assets/Scripts/menúInicial/creditManager.cs
+++ Assets/Scripts/menúInicial/creditManager.cs
@@ -6,26 +6,37 @@ using UnityEngine.UI;
 
 public class creditManager : MonoBehaviour
 {
-    public TextMeshProUGUI text;
-    public Image image;
+    [SerializeField] private TextMeshProUGUI be;
+    [SerializeField] private TextMeshProUGUI mal;
+    [SerializeField] private Image image;
 
     public bool final;
-    public string falseFinalText;
-    public string trueFinalText;
+    [SerializeField] private string falseFinalText;
+    [SerializeField] private string trueFinalText;
 
-    public Image falseFinalImage;
-    public Image trueFinalImage;
+    [SerializeField] private Image falseFinalImage;
+    [SerializeField] private Image trueFinalImage;
+
+    private void Start()
+    {
+        be.gameObject.SetActive(false);
+        mal.gameObject.SetActive(false);
+    }
 
     public void setFinal(bool finalDesbloquejat)
     {
+
         final = finalDesbloquejat;
+        
         if (final)
         {
-            text.text = trueFinalText.Replace("\\n", "\n");
+            be.gameObject.SetActive(true);
+            //text.text = trueFinalText.Replace("\\n", "\n");
             image = trueFinalImage;
         } else
         {
-            text.text = falseFinalText.Replace("\\n", "\n");
+            mal.gameObject.SetActive(true);
+            //text.text = falseFinalText.Replace("\\n", "\n");
             image = falseFinalImage;
         }
     }
